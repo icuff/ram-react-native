8a28075735b950e48f8827b4ae492936f2893181
bc-nogueira <bcnogueira@id.uff.br>
2019-06-11 22:04:44 -0300

    Adding timer.

--- App.js
+++ App.js
@@ -1,26 +1,35 @@
 import React, {Component} from 'react';
 import {Button, StyleSheet, View, ToastAndroid} from 'react-native';
+import moment from 'moment';
 
 export default class App extends Component {
   state = {
-    size: 100000,
+    size: 1000000,
+    repetitions: 100,
     showBtn: true,
   }
 
   start = () => {
+    const timerBegin = moment().valueOf();
     this.setState({
       showBtn: false
     });
+    var bar = new Promise((resolve, reject) => {
+      for (let i = 1; i <= this.state.repetitions; i++) {
+        let array = [];
+        array = Array.from({ length: this.state.size }, () =>
+          1
+        );
+        if (i == this.state.repetitions) {
+          resolve();
+        }
+      }
+    });
 
-    for (let i = 1; i <= 10; i++) {
-      ToastAndroid.show('Loop: ' + i.toString(), ToastAndroid.SHORT);
-      let array = [];
-      array = Array.from({ length: this.state.size }, () =>
-        1
-      );
-    }
-
-    ToastAndroid.show('Finished', ToastAndroid.LONG);
+    bar.then(() => {
+      const timerFinish = moment().valueOf();
+      ToastAndroid.show('Finished in ' + (timerFinish - timerBegin) + 'ms', ToastAndroid.LONG);
+    });
   }
 
   render() {
